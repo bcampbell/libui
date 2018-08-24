02a3ebc69315442fff39639f382f5a0714cbe249
Ben Campbell <ben@scumways.com>
2018-08-24 21:20:36 +1200

    add example of windows table crashing

---
+++ examples/destruc-table/main.c
@@ -0,0 +1,125 @@
+// g++ -I .. -L ../build/out main.c -lui -lgdi32 -ld2d1 -lcomctl32 -luxtheme -ldwrite -lole32
+
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <ui.h>
+
+static uiTableModel *model=NULL;
+static uiWindow *mainwin;
+static uiButton *deleteButton;
+static uiTable *table;
+
+static int numrows = 5;
+
+static int handlerNumColumns(uiTableModelHandler *h, uiTableModel *m)
+{
+	return 1;
+}
+
+static uiTableValueType handlerColumnType(uiTableModelHandler *h, uiTableModel *m, int col)
+{
+	return uiTableValueTypeString;
+}
+
+static int handlerNumRows(uiTableModelHandler *h, uiTableModel *m)
+{
+	return numrows;
+}
+
+static uiTableValue *handlerCellValue(uiTableModelHandler *h, uiTableModel *m, int row, int col)
+{
+	char buf[32];
+	assert(row<numrows);
+	sprintf(buf, "cell %d,%d\n", row,col);
+	return uiNewTableValueString(buf);
+}
+
+static void handlerSetCellValue(uiTableModelHandler *h, uiTableModel *m, int row, int col, const uiTableValue *val)
+{
+}
+
+static uiTableModelHandler handler = {
+	handlerNumColumns,
+	handlerColumnType,
+	handlerNumRows,
+	handlerCellValue,
+	handlerSetCellValue
+};
+
+static int onClosing(uiWindow *w, void *data)
+{
+	uiQuit();
+	return 1;
+}
+
+static int onShouldQuit(void *data)
+{
+	uiWindow *mainwin = uiWindow(data);
+	uiControlDestroy(uiControl(mainwin));
+	return 1;
+}
+
+static void onAdd(uiButton *button, void *data)
+{
+	++numrows;
+	uiTableModelRowInserted(model,numrows-1);
+}
+
+static uiTable *makeTable(void)
+{
+	uiTableParams params;
+	uiTable* t;
+	model = uiNewTableModel(&handler);
+	params.Model = model;
+	params.RowBackgroundColorModelColumn = -1;
+	t = uiNewTable(&params);
+	uiTableAppendTextColumn(t, "Col 0", 0, -1, NULL);
+	return t;
+}
+
+static uiControl *makeLayout(void)
+{
+	uiBox *vbox;
+	vbox = uiNewVerticalBox();
+	uiBoxSetPadded(vbox,1);
+	table = makeTable();
+	uiBoxAppend(vbox,uiControl(table),1);
+
+	uiButton* but = uiNewButton("Add");
+	uiButtonOnClicked(but, onAdd, 0);
+	uiBoxAppend(vbox, uiControl(but),0);
+	return uiControl(vbox);
+}
+
+
+int main(void)
+{
+#ifdef _WIN32
+	setbuf(stdout, NULL);
+#endif
+
+	uiInitOptions options;
+	const char *err;
+
+	memset(&options, 0, sizeof (uiInitOptions));
+	err = uiInit(&options);
+	if (err != NULL) {
+		fprintf(stderr, "error initializing libui: %s", err);
+		uiFreeInitError(err);
+		return 1;
+	}
+	mainwin = uiNewWindow("destruc-table", 640, 480, 1);
+	uiWindowOnClosing(mainwin, onClosing, NULL);
+	uiOnShouldQuit(onShouldQuit, mainwin);
+	uiControl* layout = makeLayout();
+	uiWindowSetChild(mainwin, layout);
+	uiWindowSetMargined(mainwin, 1);
+	uiControlShow(uiControl(mainwin));
+	uiMain();
+	return 0;
+}
+
